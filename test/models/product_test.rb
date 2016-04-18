9d762cd94007a58fd46a75ccdfdcc335a177f553
Viscoze <vlad.oganov@gmail.com>
2016-04-18 07:47:36 +0300

    Add Product model, add tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "products' price should be positive" do
+    product = Product.new title: "Lorem", description: "Lorem", image_url: "lorem.jpg"
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new products(:one).title, description: "Lorem",
+                          image_url: "image.jpg", price: 0.99
+
+    assert product.invalid?
+    assert_equal [""has already been taken""], product.errors[:title]
+  end
 end
